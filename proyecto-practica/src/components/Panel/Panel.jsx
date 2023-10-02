01e281484b37edac1d6d90eb1fcba19016ed3080
BABEL-SI\david.almeda <david.almeda@babelgroup.com>
2023-10-02 08:12:09 +0200

    Corregido con lo que vimos

--- proyecto-practica/src/components/Panel/Panel.jsx
+++ proyecto-practica/src/components/Panel/Panel.jsx
@@ -4,13 +4,13 @@ import Button from '../Button/Button'
 function Panel({title, children}) {
     const [bool, setBool] = useState(false)
     function handleClick() {
-        setBool(!bool)
+        setBool(p => !p)
     }
   return (
     <div>
         <h1>{title}</h1>
 
-        {bool ? children : <></>}
+        {bool && children}
         <Button onClick={() => handleClick()} className='acount' >
             <p>{bool ? "Contraer" : "Mostrar"}</p>
         </Button>

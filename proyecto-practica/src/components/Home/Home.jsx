ca168bba4af6379bd6891d51196dc8bac83f4572
BABEL-SI\david.almeda <david.almeda@babelgroup.com>
2023-10-09 09:34:50 +0200

    Mejorado con navlink

--- proyecto-practica/src/components/Home/Home.jsx
+++ proyecto-practica/src/components/Home/Home.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { Outlet } from 'react-router'
-import { useMatch  } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import './index.css'
 import Header from '../Header/Header'
 function Home() {
@@ -13,10 +13,10 @@ function Home() {
             <div className='rutas'>
                 <div className='links'>
                     <ul className='lista'>
-                        <li><a  className = {useMatch ("/home/first") ? "link active" : "link"} href='http://localhost:5173/home/first'>ENLACE 1</a></li>
-                        <li><a  className = {useMatch ("/home/second") ? "link active" : "link"} href='http://localhost:5173/home/second'>ENLACE 2</a></li>
-                        <li><a  className = {useMatch ("/home/third") ? "link active" : "link"} href='http://localhost:5173/home/third'>ENLACE 3</a></li>
-                        <li><a  className = {useMatch ("/home/fourth") ? "link active" : "link"} href='http://localhost:5173/home/fourth'>ENLACE 4</a></li>
+                        <li><NavLink to="/home/first" className={({ isActive }) => isActive ? "link active" : "link"} > ENLACE 1</NavLink></li>
+                        <li><NavLink to="/home/second" className={({ isActive }) => isActive ? "link active" : "link"} > ENLACE 2</NavLink></li>
+                        <li><NavLink to="/home/third" className={({ isActive }) => isActive ? "link active" : "link"} > ENLACE 3</NavLink></li>
+                        <li><NavLink to="/home/fourth" className={({ isActive }) => isActive ? "link active" : "link"} > ENLACE 4</NavLink></li>
                     </ul>
                 </div>
                 <div className='content'>
